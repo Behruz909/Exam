d5d4d20f21b53e4f38a4ec4eac86c8bf5fc73fb3
Behruz909 <muhayyoahmedova50@gmail.com>
2025-12-23 17:45:53 +0500

    EXAM

--- 1modulImtihon/Program.cs
+++ 1modulImtihon/Program.cs
@@ -1,10 +1,103 @@
-﻿namespace _1modulImtihon
+﻿using System.ComponentModel.Design;
+using System.Net.Http.Headers;
+
+namespace _1modulImtihon
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // 1
+            List<int> numbers = new List<int> { 1, 2, 4, 7, 10 };
+            Console.WriteLine(CountEvenNumbers(numbers));
+
+            // 2
+            List<string> words = new List<string> { "hello", "world", "apple" };
+            Console.WriteLine(ElementUzunligi words));
+
+            // 3
+            Console.WriteLine(BoshidagiElement("SalomDunyo"));
+
+            // 4
+            string result = "PDP";
+            Console.WriteLine(PDP(result));
+
+            // 5
+            Console.WriteLine(("Salom g13"));
+
+
+        }
+
+        // 1
+        static int CountEvenNumbers(List<int> list)
+        {
+            int count = 0;
+            foreach (int item in list)
+            {
+                if (item % 2 == 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        //2
+        static bool ElementUzunligi(List<string> list)
+        {
+            foreach (string item in list)
+            {
+                if (item.Length <= 5)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        //3
+        static string BoshidagiElement(string text)
+        {
+            if (text.Length < 3)
+            {
+                return text;
+            }
+            return text.Substring(0, 3);
+        }
+
+        //4
+        static bool PDP(string pdp)
+        {
+            var res string = pdp.Substring(startIndex0, length3);
+            return res == "PDP";
+        }
+
+        //5
+        static bool G13(string text)
+        {
+            if (text.Contains("g13"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
+
